f3ea78dcb077ba1bd8a81a90cf52af572a3d005a
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-08-12 21:37:03 +0600

    category section added

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,47 +1,32 @@
-import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    const navLinks = <>
-        <li><a>Item 1</a></li>
-        <li><a>Item 3</a></li>
+    const navOptions = <>
+        <li><NavLink className={({ isActive }) => isActive ? "p-3 border-0 border-b-2 border-b-green-500 mr-2 font-bold" : "p-3 mr-2"} to={'/'}>Home</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? "p-3 border-0 border-b-2 border-b-green-500 mr-2 font-bold" : "p-3 mr-2"} to={'/about'}>About</NavLink></li>
     </>
     return (
-        <>
-            <div className="navbar fixed max-w-7xl mx-auto bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            {navLinks}
-                        </ul>
+        <div className="navbar fixed z-50 bg-opacity-30 max-w-screen-xl text-white bg-red-300">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <a className="btn btn-ghost text-xl">Bistro Boss</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {navLinks}
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navOptions}
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
-                </div>
+                <a className="btn btn-ghost text-xl">Bistro<br /> Boss</a>
             </div>
-        </>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navOptions}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
     );
 };
 
